48a15779d46420534ff78cbccc5da39d419aaa5e
FMendezSlc <fmendezslc@gmail.com>
2023-03-21 14:19:12 -0600

    Day 17, the quiz project

---
+++ D17_Python_Classes/main.py
@@ -0,0 +1,18 @@
+# Intro to class definition and construction in Python
+class User:
+    
+    def __init__(self, user_id, username) -> None:
+        self.id = user_id
+        self.username = username
+        self.followers = 0
+        self.following = 0
+
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
+        
+
+
+user_1 = User('001', 'angela')
+
+print(user_1.username)
